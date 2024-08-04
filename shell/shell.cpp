03330b6443e97632023844846076d9ed0aeb0d67
esteroliver <oliver.ester.2004@gmail.com>
2024-08-04 16:08:46 -0300

    shell

--- shell/shell.cpp
+++ shell/shell.cpp
@@ -14,59 +14,65 @@
 using namespace std;
 
 void processo_comando(string comando){
-    if(comando == "pwd"){
-        cout << filesystem::current_path() << endl;
+    //argumentos
+    vector<string> args;
+    size_t start = 0;
+    size_t end = comando.find(' ');
+    while (end != string::npos) {
+        args.push_back(comando.substr(start, end - start));
+        start = end + 1;
+        end = comando.find(' ', start);
     }
-    if(comando.find("cd") == 0){
-        char diretorio[comando.length()-3];
-        for(int i = 3; i < comando.length(); i++){
-            diretorio[i-3] = comando[i];
-        }
-        chdir(diretorio);
+    args.push_back(comando.substr(start));
+    
+
+    if(args[0] == "pwd"){
+        cout << filesystem::current_path() << endl;
         return;
     }
-    else{
-        //comando
-        int caracteres_comando = 0;
-        for(int i = 0; i < comando.length(); i++){
-            if(comando[i] == ' ') break;
-            caracteres_comando++;
-        }
-        char comando_n[caracteres_comando-2];
-        for(int i = 0; i < caracteres_comando; i++){
-            comando_n[i] = comando[i];
-        }
-        string comando_nome = comando_n;
-        string caminho_absoluto = "chamadas/";
-        if(access(caminho_absoluto.c_str(), F_OK) == 0){ //verificando que o diretório existe
-            if(access(caminho_absoluto.c_str(), X_OK) == 0){ //verificando se o arquivo é executável
-                //dividindo entre processo filho e processo pai
-                pid_t pid = fork();
-                if(pid < 0){
-                    cout << "Erro de execução." << endl;
-                    return;
+    if(args[0] == "cd"){
+        chdir(args[1].c_str());
+        return;
+    }
+
+    string caminho_absoluto = "/mnt/d/desenvolvimento/repositorios/ShellSistemasOperacionais/chamadas";
+    //executar comando
+    if(access(caminho_absoluto.c_str(), F_OK) == 0){ //verificando que o diretório existe
+        if(access(caminho_absoluto.c_str(), X_OK) == 0){ //verificando se o arquivo é executável
+            //dividindo entre processo filho e processo pai
+            pid_t pid = fork();
+            if(pid < 0){
+                cout << "Erro de execução." << endl;
+                return;
+            }
+            else if(pid == 0){
+                vector<char*> argv;
+                argv.push_back((char *)args[0].c_str());
+                if(args.size() > 1){
+                    argv.push_back((char *)args[1].c_str());
+                    if(args.size() == 3)
+                        argv.push_back((char *)args[2].c_str());
                 }
-                else if(pid == 0){
-                    char * argv[2] = {(char *)comando.c_str(), nullptr};
-                    execve(caminho_absoluto.c_str(), argv, NULL);
-                }
-                else{
-                    //to do
-                }
+                argv.push_back(nullptr);
+                execvp(argv[0], argv.data());
             }
             else{
-                cout << "Permissão negada: " << comando << endl;
+                //to do
             }
         }
         else{
-            cout << "Comando não encontrado." << endl;
+            cout << "Permissão negada: " << comando << endl;
         }
+    }
+    else{
+        cout << "Comando não encontrado." << endl;
     }
 }
 
+
 int main() {
     while (true) {
-        cout << "$ ";
+        cout << "$>";
         string comando;
         getline(cin, comando);
         if(comando == "exit") break;
